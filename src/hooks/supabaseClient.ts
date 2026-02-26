e45b687da6f145e38ccf4a909db5bb0a2289aba8
Hemant Bangari <hemant.bangari@mai.io>
2026-02-26 22:57:59 +0530

    fix

--- src/hooks/supabaseClient.ts
+++ src/hooks/supabaseClient.ts
@@ -1,7 +1,7 @@
 // src/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;     // Vite example
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string || "https://bmqnnnqowkncsxvtdcop.supabase.co";     // Vite example
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string || "sb_publishable_U900QWd_doBfYK_ToEtDoQ_78r9aTL4";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
