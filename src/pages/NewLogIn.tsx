ed780a39ab2675572ff65060e56e53baa848f649
Hemant Bangari <hemant.bangari@mai.io>
2026-03-08 14:13:12 +0530

    fix

--- src/pages/NewLogIn.tsx
+++ src/pages/NewLogIn.tsx
@@ -10,7 +10,7 @@ declare global {
   }
 }
 
-const WIDGET_SCRIPT_SRC = "http://localhost:3000/my-widget.js";
+const WIDGET_SCRIPT_SRC = "https://qa-kalp-embedded-wallet.p2eppl.com/my-widget.js";
 const WIDGET_CONTAINER_ID = "kalp-wallet-container";
 const WIDGET_API_KEY = "8bfcfa2aa2d525b1ba196c6f553cda6146a73df68fd59bc3ec6ad368ab7e7237";
 const REDIRECT_URL = "/dashboard";
